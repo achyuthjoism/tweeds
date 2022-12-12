29c6cd2a32ae9726b092d54ab4823645fc5e270a
Achyuth Jois M <achyuth07042006@gmail.com>
2022-12-12 21:16:37 +0530

    added few minor change

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+import io
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
 
 setup(
     name='tweeds',
-    version='1.0',
+    version='1.1',
     author='Achyuth Jois M',
+    author_email='developer.arjm@gmail.com',
     packages=find_packages(include=['tweeds', 'tweeds.*']),
     install_requires=[
         'pandas',
@@ -11,11 +19,12 @@ setup(
     ],
     license='MIT',
     license_files=('LICENSE.md'),
+    url='https://github.com/achyuthjoism/Twitter-OSINT',
     description='An advanced Twitter scraping & OSINT tool.',
+    long_description=long_description,
     entry_points={
         'console_scripts': [
             'tweeds = tweeds.main:main',
         ]
     },
-
 )
